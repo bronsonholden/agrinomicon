de3e9730317f828e3badcd031d3ecdcc5316626e
Paul Holden <paul@paulholden.dev>
2023-09-29 13:48:34 -0700

    Update seeds.exs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -12,11 +12,35 @@
 
 Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
   kingdom: :plantae,
+  genus: "Zea",
+  species: "Mays",
+  binomial_name: "Zea mays",
+  aliases: [],
+  common_names: ["corn"],
+  cdl_value: "1",
+  geometry_color: "FFD400"
+})
+
+Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
+  kingdom: :plantae,
+  genus: "Glycine",
+  species: "Max",
+  binomial_name: "Glycine max",
+  aliases: [],
+  common_names: ["soybean"],
+  cdl_value: "5",
+  geometry_color: "267300"
+})
+
+Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
+  kingdom: :plantae,
   genus: "Vitis",
   species: "Vinifera",
   binomial_name: "Vitis vinifera",
   aliases: [],
-  common_names: ["table grape"]
+  common_names: ["table grape"],
+  cdl_value: "704489",
+  geometry_color: "704489"
 })
 
 Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
@@ -25,7 +49,9 @@ Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
   species: "C. × sinensis",
   binomial_name: "Citrus × sinensis",
   aliases: ["Citrus sinensis"],
-  common_names: ["orange"]
+  common_names: ["orange"],
+  cdl_value: "212",
+  geometry_color: "E67525"
 })
 
 Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
@@ -34,16 +60,9 @@ Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
   species: "Avium",
   binomial_name: "Prunus avium",
   aliases: [],
-  common_names: ["cherry"]
-})
-
-Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
-  kingdom: :plantae,
-  genus: "Prunus",
-  species: "Cerasus",
-  binomial_name: "Prunus cerasus",
-  aliases: [],
-  common_names: ["cherry"]
+  common_names: ["cherry"],
+  cdl_value: "66",
+  geometry_color: "FF00FF"
 })
 
 Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
@@ -52,7 +71,9 @@ Agrinomicon.Repo.insert!(%Agrinomicon.Taxonomy.Classification{
   species: "Dulcis",
   binomial_name: "Prunus dulcis",
   aliases: [],
-  common_names: ["almond"]
+  common_names: ["almond"],
+  cdl_value: "75",
+  geometry_color: "00A884"
 })
 
 Agrinomicon.Repo.insert!(%Agrinomicon.Agency.Organization{
